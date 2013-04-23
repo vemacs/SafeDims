e7a8ee99645f867d0c1901c9fefeac639e24b7ea
vemacs <gentoo@outlook.com>
2013-04-23 17:46:05 -0600

    Initial commit

---
+++ src/main/java/com/nullblock/vemacs/safedims/SafeDims.java
@@ -0,0 +1,43 @@
+package com.nullblock.vemacs.safedims;
+
+import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SafeDims extends JavaPlugin implements Listener {
+	public void onDisable() {
+	}
+
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		saveDefaultConfig();
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerTeleport(PlayerTeleportEvent event) {
+		Location to = event.getTo();
+		if (Utils.isDangerous(to.getWorld().getName()).booleanValue()) {
+			getLogger().info(
+					"Orig Y for " + event.getPlayer().getName() + " to "
+							+ to.getWorld().getName() + ": " + to.getY());
+
+			Location step = event.getTo().getBlock()
+					.getRelative(BlockFace.DOWN).getLocation();
+
+			Location safe = Utils.getSafeY(step);
+			Location adjust = new Location(to.getWorld(), safe.getX(),
+					safe.getY() + 2, safe.getZ());
+
+			getLogger().info(
+					"New Y for " + event.getPlayer().getName() + " to "
+							+ adjust.getWorld().getName() + ": "
+							+ adjust.getY());
+
+			event.setTo(adjust);
+		}
+	}
+}
